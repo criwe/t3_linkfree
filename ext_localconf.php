1fdb4fcd84ca615c034427cf0e9cdd1151743f73
criwe <criwe@users.noreply.github.com>
2021-08-26 20:24:40 +0200

    remove unneeded call

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,4 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-/***************
- * Add default RTE configuration
- */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['t3_linkfree'] = 'EXT:t3_linkfree/Configuration/RTE/Default.yaml';
 
